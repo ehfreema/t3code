fa368eb648a30cee88f5db30ddc1579e1b3b7813
Mike Olson <mwolson@member.fsf.org>
2026-07-29 22:58:13 -0400

    fix(orchestrator): Keep in-flight and stopped tool rows

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -6345,7 +6345,6 @@ function ChatViewContent(props: ChatViewProps) {
               <MessagesTimeline
                 key={activeThread.id}
                 isWorking={isWorking}
-                activeTurnInProgress={isWorking || !latestRunSettled}
                 activeTurnStartedAt={activeWorkStartedAt}
                 pendingBackgroundTasks={pendingBackgroundTasks}
                 listRef={legendListRef}
